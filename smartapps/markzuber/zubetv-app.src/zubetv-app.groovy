ee938e1f5359339d3ea884317067d5bb9660dff7
Mark Zuber <mark@zube.com>
2015-12-07 20:23:55 -0800

    Skeleton app

---
+++ smartapps/markzuber/zubetv-app.src/zubetv-app.groovy
@@ -0,0 +1,42 @@
+/**
+ *  ZubeTV App
+ *
+ *  Copyright 2015 Mark Zuber
+ *
+ */
+definition(
+    name: "ZubeTV App",
+    namespace: "MarkZuber",
+    author: "Mark Zuber",
+    description: "App to integrate with ZubeTV home automation server.",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    oauth: true)
+
+
+preferences {
+	section("Title") {
+		// TODO: put inputs here
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+// TODO: implement event handlers
